b7709659f0cca7ff2dd9aea488df9464e287f5d1
Dima <dima.trotsiuk322@gmail.com>
2022-05-13 17:39:22 +0200

    Створено таблиці groups, chats

--- utils/db_api/models.py
+++ utils/db_api/models.py
@@ -1,8 +1,8 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, String, Integer, ForeignKey
+from sqlalchemy import create_engine, Column, String, Integer, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
-engine = create_engine('sqlite:///sqlite3.db')
+engine = create_engine('sqlite:///sqlite3.db?charset=utf8mb4')
 engine.connect()
 
 Base = declarative_base()
@@ -10,16 +10,16 @@ Base = declarative_base()
 
 class Group(Base):
     __tablename__ = 'groups'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     title = Column(String(100), nullable=False)
     chats = relationship("Chat")
 
 
 class Chat(Base):
     __tablename__ = 'chats'
-    id = Column(Integer, primary_key=True)
+    chat_id = Column(Integer, unique=True, nullable=False, primary_key=True)
     title = Column(String(100), nullable=False)
-    author_id = Column(Integer, ForeignKey('groups.id'))
+    group_id = Column(Integer, ForeignKey('groups.id'))
 
 
 Base.metadata.create_all(engine)
