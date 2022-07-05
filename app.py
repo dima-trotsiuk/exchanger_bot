6fd1756e8bc2dbb9065dfb49291cbb96fdbae1b8
Dima <dima.trotsiuk322@gmail.com>
2022-07-05 21:19:14 +0200

    Поправив тротлінг

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ from utils.set_bot_commands import set_default_commands
 
 async def on_startup(dp):
     import filters
-    import middlewares
     filters.setup(dp)
-    middlewares.setup(dp)
 
     from utils.notify_admins import on_startup_notify
     await on_startup_notify(dp)
